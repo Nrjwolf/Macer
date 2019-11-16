168b85437b0a47f04a56e3752aca472ff4ae43ab
Alexander <nrjwolf@gmail.com>
2019-11-17 01:51:30 +0300

    init

---
+++ Assets/Scripts/Macer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Macer : MonoBehaviour
+{
+    private Rigidbody2D m_Rigidbody2D;
+    public Rigidbody2D Rigidbody2D { get => m_Rigidbody2D != null ? m_Rigidbody2D : m_Rigidbody2D = GetComponent<Rigidbody2D>(); }
+
+    [SerializeField] private Transform m_Eye;
+    [SerializeField] private TouchZone m_TouchZone;
+
+    void Start()
+    {
+        m_TouchZone.OnDragBegin += OnDragBegin;
+        m_TouchZone.OnDragEvent += OnDragEvent;
+        m_TouchZone.OnDragEnd += OnDragEnd;
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    private void OnDragBegin(PointerEventData pointerEvent)
+    {
+        
+    }
+
+    private void OnDragEvent(PointerEventData pointerEvent)
+    {
+        // Rigidbody2D.vel
+    }
+
+    private void OnDragEnd(PointerEventData pointerEvent)
+    {
+
+    }
+}
---
+++ Assets/Scripts/TouchZone.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class TouchZone : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public bool IsDrag { get; private set; }
+    public Action<PointerEventData> OnDragEvent = delegate { };
+    public Action<PointerEventData> OnDragBegin = delegate { };
+    public Action<PointerEventData> OnDragEnd = delegate { };
+
+    [SerializeField] private Image m_JoystickImage;
+    [SerializeField] private Image m_JoystickInner;
+
+    private Vector2 m_BeginDragPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        IsDrag = true;
+        OnDragBegin(eventData);
+
+        m_BeginDragPosition = eventData.position;
+        m_JoystickImage.GetComponent<RectTransform>().anchoredPosition = m_BeginDragPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        OnDragEvent(eventData);
+
+        var rectT = m_JoystickInner.GetComponent<RectTransform>();
+        var delta = eventData.position - m_BeginDragPosition;
+        delta = Vector2.ClampMagnitude(delta, 40);
+        rectT.anchoredPosition = delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        IsDrag = false;
+        OnDragEnd(eventData);
+
+        m_JoystickImage.gameObject.SetActive(false);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        m_JoystickImage.gameObject.SetActive(true);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        m_JoystickImage.gameObject.SetActive(false);
+        m_JoystickImage.GetComponent<RectTransform>().anchoredPosition = m_BeginDragPosition;
+    }
+}
